e791e90cb1d175f19d57a5b2735168634a076f2d
hatch <empty>
2012-06-01 21:26:43 +0000

    merged from work

--- Vec.py
+++ Vec.py
@@ -25,10 +25,10 @@ import math
 class Vec(list):
     def __init__(self,*args):
         if len(args) == 1: # e.g. Vec([x,y,z])
-            arg = args[0]
+            arg = [float(x) for x in args[0]]
             list.__init__(self,arg)
         else: # e.g. Vec(x,y,z).  This is just a convenience for more than 1 arg; to initialize a vector of length 1 you still have to say Vec([x])
-            list.__init__(self,args)
+            list.__init__(self,[float(x) for x in args])
     def __mul__(self,rhs):
         return Vec([x*rhs for x in self])
     def __rmul__(self,lhs):
@@ -38,6 +38,10 @@ class Vec(list):
     def __add__(self, rhs):
         assert len(self) == len(rhs)
         return Vec([a+b for a,b in zip(self,rhs)]) # throws if differing sizes
+    # need to override +=, otherwise we get list's
+    def __iadd__(self, rhs):
+        self = self + rhs
+        return self
     def __sub__(self, rhs):
         assert len(self) == len(rhs)
         return Vec([a-b for a,b in zip(self,rhs)]) # throws if differing sizes
@@ -49,6 +53,9 @@ class Vec(list):
             return self
         assert len(self) == len(lhs)
         return Vec([a+b for a,b in zip(lhs,self)]) # throws if differing sizes
+    def perpDot(self):
+        assert len(self) == 2
+        return Vec(-self[1],self[0])
     def cross(self,rhs):
         if len(self) == 3 and len(rhs) == 3:
             return Vec([self[1]*rhs[2]-self[2]*rhs[1],
@@ -69,6 +76,7 @@ class Vec(list):
         length = self.length()
         assert length != 0.
         return self * 1./length
+
 
 # Little test program
 if __name__ == '__main__':
