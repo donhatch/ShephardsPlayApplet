a305adb70f12d8a26806745379ff91d6a6ebc003
Don Hatch <hatch@plunk.org>
2017-01-07 02:38:09 -0800

    experimenting more with alternative gcd implementations

--- BigIntegerBenchmark/BigIntegerBenchmarkGcd.java
+++ BigIntegerBenchmark/BigIntegerBenchmarkGcd.java
@@ -124,7 +124,210 @@ class BigIntegerBenchmarkGcd
         }
     } // myGcd
 
-    // Make sure myGcd(a,b) gives the same answer as a.gcd(b) for small values.
+    private static java.math.BigInteger myGcd2(java.math.BigInteger a, java.math.BigInteger b)
+    {
+        a = a.abs();
+        b = b.abs();
+        if (a.compareTo(b) < 0)
+        {
+            // swap
+            java.math.BigInteger temp = a; a = b; b = temp;
+        }
+        while (b.signum() != 0)
+        {
+            // avoid mod if we can do it in just a handful of subtractions.
+            // hmm, seems to speed up worst case by a factor of 2.  hooray!  still not as good as builtin on worst case though.
+            if (a.bitLength()-b.bitLength() < 3) // 3 seems to be the sweet spot for this
+            {
+                while (a.compareTo(b) >= 0)
+                {
+                    if (a.bitLength()-b.bitLength() > 2) // it's more than four times as big
+                    {
+                        a = a.subtract(b.shiftLeft(2));
+                    }
+                    else if (a.bitLength()-b.bitLength() > 1) // it's more than twice as big
+                    {
+                        a = a.subtract(b.shiftLeft(1));
+                    }
+                    else
+                        a = a.subtract(b);
+                }
+            }
+            else
+            {
+                a = a.mod(b);
+            }
+            // swap
+            assert(a.signum() >= 0);
+            assert(a.compareTo(b) < 0);
+            java.math.BigInteger temp = a; a = b; b = temp;
+        }
+        return a;
+    } // myGcd2
+
+    private static java.math.BigInteger oddPart(java.math.BigInteger a)
+    {
+        int lowestSetBit = a.getLowestSetBit();
+        return lowestSetBit >= 1 ? a.shiftRight(lowestSetBit) : a;
+    }
+
+    private static java.math.BigInteger myGcd3(java.math.BigInteger a, java.math.BigInteger b)
+    {
+        a = a.abs();
+        b = b.abs();
+        if (a.compareTo(b) < 0)
+        {
+            // swap
+            java.math.BigInteger temp = a; a = b; b = temp;
+        }
+
+        //System.out.println("===========");
+        //System.out.println("a = "+a);
+        //System.out.println("b = "+b);
+        int aLowestSetBit = a.getLowestSetBit();
+        int bLowestSetBit = b.getLowestSetBit();
+        int initialShift = aLowestSetBit <= bLowestSetBit ? aLowestSetBit : bLowestSetBit; // min
+        if (initialShift >= 0) // i.e. if neither a nor b is 0
+        {
+            // downshift a and b til both odd. can be by different amounts.
+            if (aLowestSetBit > 0) a = a.shiftRight(aLowestSetBit);
+            if (bLowestSetBit > 0) b = b.shiftRight(bLowestSetBit);
+        }
+        //System.out.println("a = "+a);
+        //System.out.println("b = "+b);
+
+        while (b.signum() != 0)
+        {
+            // avoid mod if we can do it in just a handful of subtractions.
+            // hmm, seems to speed up worst case by a factor of 2.  hooray!  still not as good as builtin on worst case though.
+            if (false) // definitely no good, even in random case. would be prohibitively bad in "best" case which becomes worst
+            {
+                while (a.compareTo(b) >= 0)
+                    a = a.subtract(b);
+            }
+            else if (a.bitLength()-b.bitLength() < 3) // 3 seems to be the sweet spot for this
+            {
+                while (a.compareTo(b) >= 0)
+                {
+                    if (a.bitLength()-b.bitLength() > 2) // it's more than four times as big
+                    {
+                        a = a.subtract(b.shiftLeft(2));
+                    }
+                    else if (a.bitLength()-b.bitLength() > 1) // it's more than twice as big
+                    {
+                        a = a.subtract(b.shiftLeft(1));
+                    }
+                    else
+                        a = a.subtract(b);
+                }
+            }
+            else
+            {
+                a = a.mod(b);
+            }
+            assert(a.signum() >= 0);
+            assert(a.compareTo(b) < 0);
+
+            // If ever even numbers come up, can immediately kill them.
+            // HOWEVER, should we perhaps detect the case when we can get more mileage by
+            // *not* shifting here?  That's when b-a became relatively small.
+            // (Maybe also when b-2*a became small, etc?
+            // or even when 2*a-b became small?  Argh!)
+
+            // Fact that may or may not be of use:
+            // if we could see the actual highest order bits,
+            // we could test very quickly which of a or b-a is (probably) better,
+            // and we generally don't need an exact test.
+
+            if (a.signum() > 0)
+            {
+                if (false)
+                {
+                    // Bleah! This didn't help.  BUT... I wonder if it would be more practical
+                    // if we could do the tests using the high bits and short-circuiting early
+                    // as suggested above.  Just walk along the 3 candidates
+                    // until we get a definitive answer, or just stop when we've gone
+                    // far enough so that it's close enough to a tie that it doesn't matter much.
+                    // Although.. it seems like it's always worthwhile to keep going
+                    // if it's close to a tie, because *if* it's close to a tie
+                    // then every bit examined shaves off another bit in the computation, right?
+
+                    // So in general... shift around until we get kind of a best match
+                    // between a and b,
+                    // and see if (odd part of) the difference is better than (odd part of) a.
+                    // So let's see, there are 3 candidates:
+                    //    (1) odd part of a
+                    //    (2) odd part of b-(a<< til just < b)
+                    //    (3) odd part of b-(a<< til just > b)
+                    int aBitLength = a.bitLength();
+                    int bBitLength = b.bitLength();
+                    assert(aBitLength <= bBitLength);
+                    java.math.BigInteger aShiftedToSameSizeAsB = (aBitLength==bBitLength ? a : a.shiftLeft(bBitLength-aBitLength));
+                    java.math.BigInteger aShiftedToJustBelowB, aShiftedToJustAboveB;
+                    if (aShiftedToSameSizeAsB.compareTo(b) <= 0)
+                    {
+                        aShiftedToJustBelowB = aShiftedToSameSizeAsB;
+                        aShiftedToJustAboveB = aShiftedToJustBelowB.shiftLeft(1);
+                    }
+                    else
+                    {
+                        aShiftedToJustAboveB = aShiftedToSameSizeAsB;
+                        aShiftedToJustBelowB = aShiftedToJustAboveB.shiftRight(1);
+                    }
+
+                    java.math.BigInteger firstCandidate = oddPart(a);
+                    java.math.BigInteger secondCandidate = oddPart(b.subtract(aShiftedToJustBelowB));
+                    java.math.BigInteger thirdCandidate = oddPart(aShiftedToJustAboveB.subtract(b));
+                    a = firstCandidate;
+                    if (secondCandidate.compareTo(a) < 0)
+                        a = secondCandidate;
+                    if (thirdCandidate.compareTo(a) < 0)
+                        a = thirdCandidate;
+                }
+                else
+                {
+                    // nothing here panned out much, except just going to odd part of a seemed like a good idea
+
+                    java.math.BigInteger aSaved = a;
+                    if (false) // fooey, seems like a loss
+                    {
+                        if (a.shiftLeft(1).compareTo(b) >= 0)
+                            a = b.subtract(a);
+                    }
+                    a = oddPart(a);
+                    if (false) // fooey, seems like a loss
+                    {
+                        java.math.BigInteger aOtherPossibility = b.subtract(aSaved);
+                        if (aOtherPossibility.compareTo(a) < 0)
+                            a = aOtherPossibility;
+                    }
+                }
+            }
+
+            // swap
+            java.math.BigInteger temp = a; a = b; b = temp;
+        }
+        //System.out.println("------");
+        //System.out.println("a = "+a);
+        if (initialShift > 0)
+        {
+            a = a.shiftLeft(initialShift);
+        }
+        //System.out.println("a = "+a);
+        //System.out.println("===========");
+        return a;
+    } // myGcd3
+
+    // IDEA: for randomized case, almost never need to look very far
+    // in from the highest bits to see what's going to happen.
+    // Can we build up a sequence of relatively small operations
+    // and apply them all at once, instead of operating on a,b directly each time?
+    // Maybe as long as the coeff sizes fit in a "word" (whatever that is?)
+    // since multiplying by a word is probably faster than
+    // general multiplying.
+
+
+    // Make sure myGcd(a,b) and variations give the same answer as a.gcd(b) for small values.
     private static void myGcdConfidenceTest()
     {
         System.err.print("Running confidence test... ");
@@ -136,8 +339,16 @@ class BigIntegerBenchmarkGcd
             java.math.BigInteger b = java.math.BigInteger.valueOf(j);
             java.math.BigInteger theirAnswer = a.gcd(b);
             java.math.BigInteger myAnswer = myGcd(a, b);
+            java.math.BigInteger myAnswer2 = myGcd2(a, b);
+            java.math.BigInteger myAnswer3 = myGcd3(a, b);
             if (!myAnswer.equals(theirAnswer)) {
                 throw new AssertionError("they say gcd("+a+","+b+") is "+theirAnswer+", I say it's "+myAnswer);
+            }
+            if (!myAnswer2.equals(theirAnswer)) {
+                throw new AssertionError("they say gcd("+a+","+b+") is "+theirAnswer+", I2 say it's "+myAnswer2);
+            }
+            if (!myAnswer3.equals(theirAnswer)) {
+                throw new AssertionError("they say gcd("+a+","+b+") is "+theirAnswer+", I3 say it's "+myAnswer3);
             }
         }
         System.err.println("passed.");
@@ -146,10 +357,16 @@ class BigIntegerBenchmarkGcd
     public static void main(String args[])
     {
         boolean useMine = false;
+        boolean useMine2 = false;
+        boolean useMine3 = false;
         if (args.length==1 && args[0].equals("theirs"))
-            useMine = false;
+            ;
         else if (args.length==1 && args[0].equals("mine"))
             useMine = true;
+        else if (args.length==1 && args[0].equals("mine2"))
+            useMine2 = true;
+        else if (args.length==1 && args[0].equals("mine3"))
+            useMine3 = true;
         else
         {
             System.err.println("Usage: BigIntegerBenchmarkGcd theirs|mine");
@@ -158,18 +375,35 @@ class BigIntegerBenchmarkGcd
 
         myGcdConfidenceTest();
 
+        java.util.Random rng = new java.util.Random(0);
+
         System.out.println("# numDigits seconds");
         for (int numDigits = 1; numDigits <= (1<<20); numDigits *= 2)
         {
-            java.math.BigInteger b = java.math.BigInteger.TEN.pow(numDigits);
-            java.math.BigInteger a = b.subtract(java.math.BigInteger.valueOf(3));
+            java.math.BigInteger a, b;
+            if (false)
+            {
+                // Euclidean: best case scenario.
+                // Binary: atrocious (worst case?)
+                b = java.math.BigInteger.TEN.pow(numDigits);
+                a = b.subtract(java.math.BigInteger.valueOf(3));
+            }
+            else
+            {
+                // Euclidean: worst case scenario.
+                // Binary: actually quite a bit better than the above for some reason.
+                a = new java.math.BigInteger(numDigits, rng);
+                b = new java.math.BigInteger(numDigits, rng);
+            }
 
             System.out.print(numDigits+" ");
             System.out.flush();
 
             long t0nanos = System.nanoTime();
-            java.math.BigInteger aInverse = useMine ? myGcd(a, b)
-                                                    : a.gcd(b);
+            java.math.BigInteger aInverse = useMine2 ? myGcd2(a, b)
+                                          : useMine3 ? myGcd3(a, b)
+                                          : useMine  ? myGcd(a, b)
+                                                     : a.gcd(b);
             long t1nanos = System.nanoTime();
 
             double seconds = (t1nanos-t0nanos)/1e9;
